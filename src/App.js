c67b026f93e4210d6650475503eed7b5eb53a434
nic-brown <crazyfurymrx@gmail.com>
2022-06-02 15:10:25 +0300

    9. React router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 //import "./App.css";
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
+//Router
+import { Switch, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+
+      <Routes>
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/work" element={<OurWork />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
